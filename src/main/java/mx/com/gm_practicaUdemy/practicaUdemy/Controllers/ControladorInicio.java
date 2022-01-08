2863c69f8a327668b9450d2915282655cc5bb088
DanielaAvendano <daniela.avendano.mt@correo.usa.edu.co>
2022-01-08 11:38:42 -0500

    Agregando seguridad con Spring

--- src/main/java/mx/com/gm_practicaUdemy/practicaUdemy/Controllers/ControladorInicio.java
+++ src/main/java/mx/com/gm_practicaUdemy/practicaUdemy/Controllers/ControladorInicio.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import mx.com.gm_practicaUdemy.practicaUdemy.Entities.Persona;
 import mx.com.gm_practicaUdemy.practicaUdemy.Services.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -23,10 +25,11 @@ public class ControladorInicio {
     private PersonaService personaService;
     
     @GetMapping("/")
-    public String inicio(Model model){
+    public String inicio(Model model, @AuthenticationPrincipal User user){
         
         var personas = personaService.listarPersonas();
         log.info("ejecutando el controlador Spring MVC");
+        log.info("usuario que hizo login:"+ user);
         model.addAttribute("personas", personas);
         return "index";
     }
